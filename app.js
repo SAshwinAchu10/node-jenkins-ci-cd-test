8d6cc1bb3e773a370c9be268549603bd7e18ecd3
Ashwin <achu10@live.in>
2020-01-13 17:03:38 +0530

    .17

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require('express');
 var app = express();
 //Respoand with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world');
+ res.send('hello worldu');
 });
 
 app.get('/ping', function (req, res) {
- res.send('pong 0.0.17');
+ res.send('pong 0.0.18');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 8080);
