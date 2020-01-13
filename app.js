4c9fb7e3fdefba0e6eb05d4b937a89aea19aae95
Ashwin <achu10@live.in>
2020-01-13 17:02:23 +0530

    .17

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ping', function (req, res) {
- res.send('pong 0.0.16');
+ res.send('pong 0.0.17');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 8080);
