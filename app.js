80d0f46adfd1be01bc8ee51408604243704a365c
Ashwin Achu <59597975+SAshwinAchu10@users.noreply.github.com>
2020-01-13 16:42:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ping', function (req, res) {
- res.send('poang 2kq1a');
+ res.send('poang 2kqa1a');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 8080);
