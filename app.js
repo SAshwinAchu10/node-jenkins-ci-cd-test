99f9111dbdd86e67dc0bd2c9aa9ae8393a273e65
Ashwin Achu <59597975+SAshwinAchu10@users.noreply.github.com>
2020-01-13 14:49:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world 3');
+ res.send('hello world');
+});
+
+app.get('/ping', function (req, res) {
+ res.send('pong');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
